38fa7728d4d2bb167f5af20f61bda783bf496d37
Scott Collier <scott@onlinespaces.com>
2016-01-02 22:57:44 +0000

    Add facebook controller

---
+++ src/AppBundle/Controller/FacebookController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class FacebookController extends Controller
+{
+    /**
+     * @Route("/connect/facebook", name="connect_facebook")
+     */
+    public function connectFacebookAction(Request $request)
+    {
+        return $this->get('app_facebook_authenticator')->start($request);
+    }
+    
+    /**
+     * @Route("/connect/facebook-check", name="connect_facebook_check")
+     */
+    public function secureAction()
+    {
+        // will not be reached!
+    }
+    
+}
